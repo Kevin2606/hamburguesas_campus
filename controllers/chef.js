c3af4d106fcb79f1fe3b19226666dabfa19255b2
Kevin Gallardo <andreskevin2606@gmail.com>
2023-09-06 16:46:00 -0500

    feat: :sparkles: Se crea endpoints y metodos estaticos para chef

--- controllers/chef.js
+++ controllers/chef.js
@@ -1,5 +1,16 @@
 import { ChefModel } from "../models/chef.js";
 
 export class ChefController {
-
+    static async getEspecialidad(req, res) {
+    }
+    static async actualizarEspecialidad(req, res) {
+    }
+    static async getContar(req, res) {
+    }
+    static async postNuevo(req, res) {
+    }
+    static async getListarExcepto(req, res) {
+    }
+    static async deleteVegetariana(req, res) {
+    }
 }
--- routes/chefsRoute.js
+++ routes/chefsRoute.js
@@ -3,6 +3,22 @@ import { ChefController } from "../controllers/chef.js";
 
 const router = Router();
 
+/*
+3. Encontrar todos los **chefs** que se especializan en "Carnes"
+10. Cambiar la especialidad del "ChefC" a "Cocina Internacional" - **CHEFS**
+16. Contar cuántos **chefs** hay en la base de datos
+19. Agregar un nuevo **chef** a la colección con una especialidad en "Cocina Asiática"
+24. Listar todos los **chefs** excepto "ChefA"
+29. Eliminar todos los **chefs** que tienen una especialidad en "Cocina Vegetariana"
+*/
+router
+.get('/especialidad', ChefController.getEspecialidad)
+.patch('/actualizar-especialidad', ChefController.actualizarEspecialidad)
+.get('/contar', ChefController.getContar)
+.post('/nuevo', ChefController.postNuevo)
+.get('/listar-excepto', ChefController.getListarExcepto)
+.delete('/vegetariana', ChefController.deleteVegetariana);
+
 
 export default router;
 
