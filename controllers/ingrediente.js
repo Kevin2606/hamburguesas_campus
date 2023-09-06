36807b158495a8fe51b21e75370c9df9eae62122
Kevin Gallardo <andreskevin2606@gmail.com>
2023-09-06 16:37:12 -0500

    feat: :sparkles: Se crea metodos estaticos en el controlador de ingredientes

--- controllers/ingrediente.js
+++ controllers/ingrediente.js
@@ -1,5 +1,22 @@
 import { IngredienteModel } from "../models/ingrediente.js";
 
 export class IngredienteController {
-
+    static async getStock(req, res) {
+    }
+    static async aumentarPrecio(req, res) {
+    }
+    static async deleteStock0(req, res) {
+    }
+    static async incrementarStock(req, res) {
+    }
+    static async getClasico(req, res) {
+    }
+    static async getPrecio(req, res) {
+    }
+    static async actualizarDescripcion(req, res) {
+    }
+    static async getAlfabetico(req, res) {
+    }
+    static async getNoHamburguesa(req, res) {
+    }
 }
